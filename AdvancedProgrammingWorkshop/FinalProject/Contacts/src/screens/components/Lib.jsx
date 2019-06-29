5d7d01020443bf2d19e28626fa03a64d8b0680e4
Ali Alizadeh <alizadeh.ali.e@gmail.com>
2019-06-29 17:41:24 +0430

    removed uneccesary files from contacts project.

--- AdvancedProgrammingWorkshop/FinalProject/Contacts/src/screens/components/Lib.jsx
+++ AdvancedProgrammingWorkshop/FinalProject/Contacts/src/screens/components/Lib.jsx
@@ -1,7 +1,5 @@
 export { default as TopAppBar } from './TopAppBar';
 export { default as AppBarText } from './AppBarText';
-export { default as ContainedButton } from './ContainedButton';
-export { default as OutlinedButton } from './OutlinedButton';
 export { default as TextButton } from './TextButton';
 export { default as Icon } from './Icon';
 export { default as CustomText } from './CustomText';
